15b67c96c5e5e7852156dd1540f4c91d68862e60
MikePeleah <mike.peleah@gmail.com>
2014-08-13 15:28:06 +0300

    Programming Assignment 2
    Programming assignment 2, including 2 functions:
    makeCacheMatrix
    cacheSolve
    
    Version 13 Aug 2014

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	m1 <- NULL
+	set <- function(y) {
+		x <<- y       ## put it in x using superassignment http://article.gmane.org/gmane.comp.lang.r.general/227066
+		m1 <<- NULL   ## clear inverse of matrix, as we don't know it
+	}
+	
+	get <- function() x    ## simply return x
+	
+	getinverse <- function() m1   ## simply return m1
 
+	setinverse <- function(minv) m1 <<- minv    ## set inverse of matrix using what we got
+	## now construct object
+	list (set = set,
+	      get = get,
+		  getinverse = getinverse,
+		  setinverse = setinverse )
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()   ## get inverse
+        if(!is.null(m)) {     ## check if it not NULL -- something is cacehd
+                message("Cached:")
+                return(m)
+        }
+		## if nothing cached...
+        data <- x$get()     ## get data 
+        m <- solve(data)    ## calculate inverse matrix
+        x$setinverse(m)     ## put it back
+        m                   ## print it
 }
